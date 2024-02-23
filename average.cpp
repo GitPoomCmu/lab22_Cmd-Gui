495e443f1ae516456e97e223b9d4b8da21802070
GitPoomCmu <152594220+GitPoomCmu@users.noreply.github.com>
2024-02-23 17:08:59 +0700

    Update average.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
 
+int main(int argc, char *argv[])
+{
+    int total=0, num = argc-1; 
+    for(int i=0; i<argc; i++){
+        total += atoi(argv[i]);
+    }
+    
+    if(num < 1) cout << "Please input numbers to find average.\n";
+    else{
+    cout << "---------------------------------\n";
+    cout << "Average of " << num << " numbers = " << total/num <<"\n";
+    cout << "---------------------------------\n";
+    }
+    
+    return 0;
+}
